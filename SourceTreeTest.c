3ac1b327bc3ee317b7ce20b3ced6ddb724c066b8
KyeongTaek <tommyholic@naver.com>
2021-11-02 17:18:02 +0900

    Test 브랜치에 코드를 추가함

--- SourceTreeTest.c
+++ SourceTreeTest.c
@@ -2,8 +2,13 @@
 
 int main()
 {
+    int i = 0;
+
     printf("First OSS code\n");
     printf("Second OSS code\n");
 
+    for (int i = 0; i < 10; i++){
+      print("Test 브랜치\n");
+    }
     return 0;
 }
